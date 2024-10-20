2b2a59916f2b5bcc19a25604f05c20db59f68a9f
TheBrettYT <bpacey06@gmail.com>
2024-10-20 05:44:21 -0500

    Again made a mistake

--- checksite.js
+++ checksite.js
@@ -4,9 +4,9 @@ setTimeout(() => {
         document.getElementById('vercel').remove()
     } else if(window.location.hostname == 'thebrettyt.vercel.app') {
         document.getElementById('github').remove()
-        document.getElementById('freebsd').remove()
+        document.getElementById('freedns').remove()
     } else if(window.location.hostname == 'thebrettyt.github.io') {
         document.getElementById('vercel').remove()
-        document.getElementById('freebsd').remove()
+        document.getElementById('freedns').remove()
     }
 }, 100);
